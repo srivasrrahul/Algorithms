70b470d0133f9bb99d9cb61825785e12ba93b8d8
Rahul <rasrivastava@paypal.com>
2015-02-17 09:57:23 +0530

    fixed hackercup 2015  problem prime problem.

--- src/Primacity.java
+++ src/Primacity.java
@@ -2,282 +2,133 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.math.BigInteger;
-import java.util.*;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
 
-class PrimeSieve implements Runnable {
 
-    private final int low;
-    private final int high;
-
-    PrimeSieve(int low,int high) {
-        this.low = low;
-        this.high = high;
-        thread = new Thread(this,"test");
-        thread.start();
-
-    }
-
-    boolean isPrime(int n) {
-        if ((n % 2) == 0) {
-            return false;
-        }
-
-        BigInteger bigInteger = BigInteger.valueOf(n);
-
-        if (bigInteger.isProbablePrime(8)) {
-            for (int i = 3;i<n;i+=2) {
-                if ((n%i) == 0) {
-                    return false;
-                }
-            }
-        }else {
-            return false;
-        }
-
-        return true;
-    }
-
-    private Thread thread;
-    private HashSet<Integer> primeNumbers = new HashSet<>();
-    @Override
-    public void run() {
-        for (int i = low;i<=high;++i) {
-            if (isPrime(i)) {
-                primeNumbers.add(i);
-            }
-        }
-    }
-
-    Set<Integer> getPrimes() {
-        return primeNumbers;
-    }
-
-    Thread getThread() {
-        return thread;
-    }
-}
-class PrimesDB {
-    private HashSet<Integer> primeNumbers = new HashSet<>();
-    private int maxPrime = 2;
-    PrimesDB() {
-        primeNumbers.add(2);
-    }
-
-    Set<Integer> getIntegers() {
-        return primeNumbers;
-    }
-
-    boolean isPrime(int n) {
-        if ((n % 2) == 0) {
-            return false;
-        }
-
-        BigInteger bigInteger = BigInteger.valueOf(n);
-
-        int x = (int)Math.sqrt(n);
-        if (bigInteger.isProbablePrime(8)) {
-            for (int i = 3;i<=x;i+=2) {
-                if ((n%i) == 0) {
-                    return false;
-                }
-            }
-        }else {
-            return false;
-        }
-
-        primeNumbers.add(n);
-        return true;
-    }
-    void extendDB(int i) {
-//        for (int j = 3;j<=i;++j) {
-//            isPrime(j);
-//        }
-
-        PrimeSieve primeSieve[] = new PrimeSieve[THREADS];
-        int eachThreadJob = (i-2)/THREADS;
-        int low = 2;
-
-        for (int j = 0;j<primeSieve.length;++j) {
-            if (j == primeSieve.length-1) {
-                System.out.println("Creating sieve for " + low + " " + i);
-                primeSieve[j] = new PrimeSieve(low,i);
-            }else {
-                System.out.println("Creating sieve for " + low + " " + (low+eachThreadJob));
-                primeSieve[j] = new PrimeSieve(low,low + eachThreadJob);
-                low = low + eachThreadJob;
-            }
-        }
-
-        for (int j = 0;j<primeSieve.length;++j) {
-            try {
-                primeSieve[j].getThread().join();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-
-        //Now merge all the sets
-        for (int j = 0;j<primeSieve.length;++j) {
-            primeNumbers.addAll(primeSieve[j].getPrimes());
-        }
-
-    }
-
-    private static final int THREADS = 8;
-
-}
-
-class PrimeFactors implements Runnable{
-    PrimeFactors(PrimesDB primesDB,int a,int b) {
-        this.primesDB = primesDB;
-        this.low = a;
-        this.high = b;
-        thread = new Thread(this,"test");
-        thread.start();
-
-    }
-
-    Thread getThread() {
-        return thread;
-    }
-
-    Thread thread;
-    private PrimesDB primesDB;
-    private int low;
-    private int high;
-
-    private HashMap<Integer,Integer> primacity = new HashMap<>();
-
-    @Override
-    public void run() {
-        for (int i = low;i<=high;++i) {
-            int samplePrimacity = 0;
-            for (int prime : primesDB.getIntegers()) {
-                if (prime > i) {
-                    break;
-                }
-
-                if ((i % prime) == 0) {
-                    ++samplePrimacity;
-                }
-            }
-
-            primacity.put(i,samplePrimacity);
-        }
-    }
-
-    HashMap<Integer,Integer> getResult() {
-        return primacity;
-    }
-}
 public class Primacity {
 
+    HashSet<Integer> primeSet = new HashSet<>();
+    LinkedList<Integer> primeLst = new LinkedList();
+    HashMap<Integer,Integer> primeMultiples = new HashMap<>();
+    void formPrimeMultiples(int max,int min) {
+        for (Integer c : primeLst) {
+            int val = c + c;
+            while (val < min) {
+                val = val + c;
+            }
+            //System.out.println("For prime " + c);
+            while (val <= max && val >=min) {
+
+                if (primeMultiples.containsKey(val)) {
+                    //System.out.println("Adding val + " + val + " " + primeMultiples.get(val)+1 );
+                    primeMultiples.put(val,primeMultiples.get(val)+1);
+                }else {
+                    //System.out.println("Adding val + " + val + " " + 1 );
+                    primeMultiples.put(val,1);
+                }
+
+                val = val + c;
+            }
+        }
+    }
+    boolean isPrime(int n) {
+        //System.out.println("Inside prime " + n);
+
+        if (n == 2) {
+            return true;
+        }
+
+        if ((n % 2) == 0) {
+            return false;
+        }
+
+        BigInteger bigInteger = BigInteger.valueOf(n);
+        if (bigInteger.isProbablePrime(8)) {
+            int x = (int)Math.sqrt(n);
+            for (int i = 2;i<=x;++i) {
+                if ((n % i) == 0) {
+
+                    return false;
+                }
+            }
+
+
+            return true;
+        }
+
+        return false;
+    }
+    int primacity(int n) {
+        //System.out.println("Inside primacity " + n);
+        if (primeSet.contains(n)) {
+            //System.out.println("n is prime " + n);
+            return 1;
+        }
+
+        int count = 0;
+        for (Integer x : primeLst) {
+            if (x < n) {
+                if ((n % x) == 0) {
+                    ++count;
+                }
+            }else {
+                break;
+            }
+        }
+
+        return count;
+
+    }
+
+
+    int findPrimacity(int a,int b,int k) {
+        //System.out.println(a + " " + b  + " " + k);
+        //primeLst.clear();
+        for (int i = (primeLst.size()>0)?(primeLst.getLast()+1):2;i<=b;++i) {
+            boolean rc = isPrime(i);
+            if (rc) {
+                //System.out.println("Adding to prime list " + i);
+                primeSet.add(i);
+                primeLst.add(i);
+            }
+            //System.out.println(isPrime(i) + " " + i);
+        }
+
+        primeMultiples.clear();
+
+        formPrimeMultiples(b,a);
+        int count = 0;
+        for (Integer key : primeMultiples.keySet()) {
+            if (primeMultiples.get(key) == k) {
+                ++count;
+            }
+        }
 
 
 
+        return count;
+    }
 
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws IOException {
         Primacity primacity = new Primacity();
         //System.out.println(primacity.checkPrimacityOfNumbers(1000000, 1000000, 1));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String line;
-        try {
+        String line = br.readLine();
+        int testCases = Integer.parseInt(line);
+        for (int i = 0;i<testCases;++i) {
             line = br.readLine();
-            int testCases = Integer.parseInt(line);
-            int maxRange = 0;
-            int minRange = 2;
-            ArrayList<String> strings = new ArrayList<>();
-            for (int i = 0;i<testCases;++i) {
-                line = br.readLine();
-                strings.add(line);
-                String data [] = line.split(" ");
-                //int max = Integer.parseInt(data[2]);
-                //primacity.updatePrimesDB(max);
-                int localMax = Integer.parseInt(data[1]);
-                if (localMax > maxRange) {
-                    maxRange = localMax;
-                }
-
-                int localMin = Integer.parseInt(data[0]);
-                if (localMin <= minRange) {
-                    minRange = localMin;
-                }
-//                System.out.println("Case #" + (i+1) + ": " +  primacity.checkPrimacityOfNumbers(Integer.parseInt(data[0]),
-//                                                                     Integer.parseInt(data[1]),
-//                                                                     Integer.parseInt(data[2])));
-            }
-
-            System.out.println(minRange);
-            System.out.println(maxRange);
-            PrimesDB primesDB = new PrimesDB();
-            primesDB.extendDB(maxRange);
-            System.out.println("Extended DB");
-
-            //Now we have primes
-            //Spawn 4 threads
-            PrimeFactors primeFactors[ ] = new PrimeFactors[THREADS];
-
-            int eachThreadJob = (maxRange - minRange)/THREADS;
-            for (int i = 0;i<THREADS;++i) {
-                System.out.println("MinRange = " + minRange + " maxRange " + (minRange + eachThreadJob));
-                if (i == THREADS -1) {
-                    //For last encompass everything
-                    primeFactors[i] = new PrimeFactors(primesDB, minRange, maxRange);
-                }else {
-                    primeFactors[i] = new PrimeFactors(primesDB, minRange, minRange + eachThreadJob);
-                }
-                minRange = minRange + eachThreadJob;
-            }
-
-            for (int i = 0;i<THREADS;++i) {
-                primeFactors[i].getThread().join();
-            }
-
-            System.out.println("Finished threads now iterated");
-            for (int i = 0;i<strings.size();++i) {
-                System.out.println("Iterating for i = " + i);
-                String [] data = strings.get(i).split(" ");
-                int low = Integer.parseInt(data[0]);
-                int high = Integer.parseInt(data[1]);
-                int givenPrimacity = Integer.parseInt(data[2]);
-                int calcPrimacity = 0;
-                for (int k = low;k<=high;++k) {
-                    if (primeFactors[0].getResult().containsKey(k)) {
-                        if (primeFactors[0].getResult().get(k) == givenPrimacity) {
-                            ++calcPrimacity;
-                        }
-                    }
-
-                    if (primeFactors[1].getResult().containsKey(k)) {
-                        if (primeFactors[1].getResult().get(k) == givenPrimacity) {
-                            ++calcPrimacity;
-                        }
-                    }
-
-                    if (primeFactors[2].getResult().containsKey(k)) {
-                        if (primeFactors[2].getResult().get(k) == givenPrimacity) {
-                            ++calcPrimacity;
-                        }
-                    }
-
-                    if (primeFactors[3].getResult().containsKey(k)) {
-                        if (primeFactors[3].getResult().get(k) == givenPrimacity) {
-                            ++calcPrimacity;
-                        }
-                    }
-                }
-
-                System.out.println(calcPrimacity);
-
-            }
-
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+            String []data = line.split(" ");
+            int a = Integer.parseInt(data[0]);
+            int b = Integer.parseInt(data[1]);
+            int k = Integer.parseInt(data[2]);
+            System.out.println("Case #" + (i + 1) + ":" + " " + primacity.findPrimacity(a, b, k));
         }
+
+
     }
 
-    private static final int THREADS = 4;
+
 }
